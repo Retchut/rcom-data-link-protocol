10e7329dd1df2c03ebf4f1c87caf3f96178f73f6
retchut <retchut@protonmail.ch>
2021-11-24 13:30:27 +0000

    feat: add frame header defines

--- src/ll.h
+++ src/ll.h
@@ -8,6 +8,20 @@
 #define RECEIVER FALSE
 #define TRANSMITTER TRUE
 
+#define BIT(shift) 1 << shift
+
+#define FLAG        0x7E
+#define A_WRT_CMD   0x03
+#define A_RCV_RESP  0x03
+#define A_RCV_CMD   0x01
+#define A_WRT_RESP  0x01
+#define C_INF(n)    (n << 6)
+#define C_SET       0x03
+#define C_DISC      0x0B
+#define C_UA        0x07
+#define C_RR(n)     (n << 7) | 0x05
+#define C_REJ(n)    (n << 7) | 0x01
+
 int llopen(int port, bool transmitter);
 
 int llclose(int fd);
@@ -15,3 +29,5 @@ int llclose(int fd);
 int llwrite(int fd, char * buffer, int length);
 
 int llread(int fd, char * buffer);
+
+int writeFrame(char *buffer, int length);
