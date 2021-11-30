15d90c5d073e6895280301f6add40b74921dbf4d
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-11-30 16:31:47 +0000

    Removes function to other files. Implements llopen

--- src/ll.c
+++ src/ll.c
@@ -1,87 +1,11 @@
-#include <fcntl.h>
 #include <stdio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <termios.h>
-#include <time.h>
-
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
+#include "defines.h"
 #include "ll.h"
+#include "read.h"
 #include "state.h"
-
-unsigned char buildBCC2(unsigned char *data, size_t size) {
-  unsigned char bcc2 = data[0];
-
-  for (size_t p = 0; p < size; p++) {
-    bcc2 ^= data[p];
-  }
-
-  return bcc2;
-}
-
-int writeInformationFrame(int fd, unsigned char addr, unsigned char cmd,
-                          unsigned char *infoPtr, size_t infoSize) {
-
-  unsigned char frame[I_FRAME_SIZE(infoSize)];
-
-  frame[0] = FLAG;
-  frame[1] = addr;
-  frame[2] = cmd;
-  frame[3] = BCC1(addr, cmd);
-
-  // check if we're not building an information frame
-  memcpy(frame + 4, infoPtr, infoSize);
-  frame[infoSize + 4] = buildBCC2(infoPtr, infoSize);
-  frame[infoSize + 5] = FLAG;
-
-  return write(fd, frame, infoSize + 6);
-}
-
-int writeSupervisionFrame(int fd, unsigned char msg_addr,
-                          unsigned char msg_ctrl) {
-  unsigned char buf[SU_FRAME_SIZE];
-
-  buf[0] = FLAG;
-  buf[1] = msg_addr;
-  buf[2] = msg_ctrl;
-  buf[3] = BCC1(msg_addr, msg_ctrl);
-  buf[4] = FLAG;
-
-  return write(fd, &buf, SU_FRAME_SIZE);
-}
-
-int readSupervisionFrame(int fd) {
-
-  unsigned char buf;
-
-  int ret = -1;
-
-  set_state(START);
-  time_t start_time = time(NULL), end_time = time(NULL);
-
-  while (difftime(end_time, start_time) < 3 && get_state() != STOP) {
-
-    ret = read(fd, &buf, 1);
-
-    printf("I read a %c\n", buf);
-    if (ret == -1) {
-      perror("read");
-      exit(-1);
-    }
-    if (ret == 0) {
-      end_time = time(NULL);
-    } else {
-      end_time = time(&start_time);
-    }
-
-    handleState(buf);
-  }
-
-  return difftime(end_time, start_time) < 3 ? SU_FRAME_SIZE : -1;
-}
+#include "write.h"
 
 int llopen(int fd, bool role) {
   if (role == TRANSMITTER) {
--- src/ll.h
+++ src/ll.h
@@ -1,49 +1,7 @@
+#ifndef LL_H_
+#define LL_H_
+
 #include <stdbool.h>
-#include <stddef.h>
-
-#define BAUDRATE B38400
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define FALSE 0
-#define TRUE 1
-
-#define RECEIVER FALSE
-#define TRANSMITTER TRUE
-
-#define SUPERVISION FALSE
-#define UNNUMERATED FALSE
-#define INFORMATION TRUE
-
-#define BIT(shift) 1 << shift
-
-#define SU_FRAME_SIZE 5
-#define I_FRAME_SIZE(s) 6 + s
-
-#define FLAG 0x7E
-#define A_SEND_CMD_ADDR 0x03
-#define A_RECV_RESP_ADDR 0x03
-#define A_RECV_CMD_ADDR 0x01
-#define A_SEND_RESP_ADDR 0x01
-#define C_INF(n) (n << 6)
-#define C_SET 0x03
-#define C_DISC 0x0B
-#define C_UA 0x07
-#define C_RR(n) (n << 7) | 0x05
-#define C_REJ(n) (n << 7) | 0x01
-#define BCC1(a, c) a ^ c
-
-unsigned char buildBCC2(unsigned char *data, size_t size);
-
-int buildFrame(unsigned char *frame, unsigned char addr, unsigned char cmd,
-               unsigned char *infoPtr, size_t infoSize);
-
-int writeSupervisionFrame(int fd, unsigned char msg_addr,
-                          unsigned char msg_ctrl);
-
-int writeInformationFrame(int fd, unsigned char msg_addr,
-                          unsigned char msg_ctrl, unsigned char *info_ptr,
-                          size_t info_size);
-
-int readSupervisionFrame(int fd);
 
 int llopen(int fd, bool transmitter);
 
@@ -52,3 +10,5 @@ int llclose(int fd);
 int llwrite(int fd, unsigned char *buffer, int length);
 
 int llread(int fd, unsigned char *buffer);
+
+#endif // LL_H_
