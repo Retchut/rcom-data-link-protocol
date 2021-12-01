5b9d2a2953dc7a60d0d0cede47163c56aab9f177
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-12-01 18:07:04 +0000

    Adds constants for parsing RR and REJ

--- src/defines.h
+++ src/defines.h
@@ -13,11 +13,13 @@
 #define BIT(shift) 1 << shift
 
 #define SU_FRAME_SIZE 5
-#define I_FRAME_SIZE(s) 6 + s
+#define I_FRAME_SIZE(s) 7 + s
 
 #define NUM_TRIES 3
 
 #define FLAG 0x7E
+#define ESCAPE 0x7d
+#define REJECTED -2
 #define A_SEND_CMD 0x03
 #define A_SEND_RSP 0x01
 #define A_RECV_CMD 0x01
@@ -25,9 +27,14 @@
 #define C_INF(n) (n << 6)
 #define C_SET 0x03
 #define C_DISC 0x0B
+#define C_RR0 0x05
+#define C_RR1 0x85
+#define C_REJ0 0x01
+#define C_REJ1 0x81
 #define C_UA 0x07
 #define C_RR(n) (n << 7) | 0x05
 #define C_REJ(n) (n << 7) | 0x01
+#define C_S(n) (((n) % 2) << 6)
 #define BCC1(a, c) a ^ c
 
 #endif // DEFINES_H_
