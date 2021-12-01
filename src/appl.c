ef3634f15971d424e5795c29d2523e30e143abf9
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-12-01 13:10:37 +0000

    Changes some variable types to have no warnings

--- src/appl.c
+++ src/appl.c
@@ -1,125 +1,131 @@
 #include <stdio.h>
-#include <sys/stat.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #include "appl.h"
 #include "ll.h"
 
-int retrieveFileData(struct fileData *fData, FILE *filePtr, char *fileName){
-    struct stat fileStat;
-    if(fstat(fileno(filePtr), &fileStat) != 0){
-        perror("fstat");
-        return 1;
-    }
+int retrieveFileData(struct fileData *fData, FILE *filePtr, char *fileName) {
+  struct stat fileStat;
+  if (fstat(fileno(filePtr), &fileStat) != 0) {
+    perror("fstat");
+    return 1;
+  }
 
-    fData->filePtr = filePtr;
-    fData->fileName = fileName;
-    fData->fileNameSize = (unsigned int) strlen(fileName);
-    fData->fileSize = (unsigned int) fileStat.st_size;
-    fData->fullPackets = fData->fileSize / DATA_CHUNK_SIZE;
-    fData->leftover = fData->fileSize % DATA_CHUNK_SIZE;
+  fData->filePtr = filePtr;
+  fData->fileName = fileName;
+  fData->fileNameSize = (unsigned int)strlen(fileName);
+  fData->fileSize = (unsigned int)fileStat.st_size;
+  fData->fullPackets = fData->fileSize / DATA_CHUNK_SIZE;
+  fData->leftover = fData->fileSize % DATA_CHUNK_SIZE;
 
-    return 0;
+  return 0;
 }
 
-int generateControlPacket(unsigned char *ctrlPacket, struct fileData *fData, int start){
-    if(start != PACKET_CTRL_START && start != PACKET_CTRL_END){
-        return -1;
-    }
-    
-    //Control field
-    ctrlPacket[0] = start;
-    //TLV1
-    ctrlPacket[1] = CTRL_FILE_SIZE;
-    ctrlPacket[2] = UNSIGNED_INT_SIZE; //handles file size up to around 4gb
-    memcpy(ctrlPacket+3, &(fData->fileSize), UNSIGNED_INT_SIZE);    //TODO; check if this is fine
-    //TLV2
-    ctrlPacket[3+UNSIGNED_INT_SIZE] = CTRL_FILE_NAME;
-    memcpy(ctrlPacket+4+UNSIGNED_INT_SIZE, &(fData->fileNameSize), 1);    //TODO; check if this is fine
-    ctrlPacket[5+UNSIGNED_INT_SIZE] = fData->fileName;
+int generateControlPacket(unsigned char *ctrlPacket, struct fileData *fData,
+                          int start) {
+  if (start != PACKET_CTRL_START && start != PACKET_CTRL_END) {
+    return -1;
+  }
 
-    return 0;
+  // Control field
+  ctrlPacket[0] = start;
+  // TLV1
+  ctrlPacket[1] = CTRL_FILE_SIZE;
+  ctrlPacket[2] = UNSIGNED_INT_SIZE; // handles file size up to around 4gb
+  memcpy(ctrlPacket + 3, &(fData->fileSize),
+         UNSIGNED_INT_SIZE); // TODO; check if this is fine
+  // TLV2
+  ctrlPacket[3 + UNSIGNED_INT_SIZE] = CTRL_FILE_NAME;
+  memcpy(ctrlPacket + 4 + UNSIGNED_INT_SIZE, &(fData->fileNameSize),
+         1); // TODO; check if this is fine
+  ctrlPacket[5 + UNSIGNED_INT_SIZE] = *fData->fileName;
+
+  return 0;
 }
 
-void generateDataPacket(unsigned char *dataPacket, struct fileData *fData, unsigned char *data, unsigned int dataSize, int seqN){
-    //TODO; check if these l1 and l2 are correct
-    unsigned int l1 = DATA_CHUNK_SIZE % 256;
-    unsigned int l2 = DATA_CHUNK_SIZE / 256;
-    unsigned int dataPacketHeader[4] = {PACKET_DATA, seqN, l1, l2};
-    memcpy(dataPacket, dataPacketHeader, 4);
-    memcpy(dataPacket+4, data, dataSize);
+void generateDataPacket(unsigned char *dataPacket, struct fileData *fData,
+                        unsigned char *data, unsigned int dataSize, int seqN) {
+  // TODO; check if these l1 and l2 are correct
+  unsigned int l1 = DATA_CHUNK_SIZE % 256;
+  unsigned int l2 = DATA_CHUNK_SIZE / 256;
+  unsigned int dataPacketHeader[4] = {PACKET_DATA, seqN, l1, l2};
+  memcpy(dataPacket, dataPacketHeader, 4);
+  memcpy(dataPacket + 4, data, dataSize);
 }
 
-int write(int portfd, char *fileName){
-    FILE *filePtr = fopen(fileName, "r");
-    if(filePtr == NULL){
-        perror("fopen");
-        return 1;
+int write(int portfd, char *fileName) {
+  FILE *filePtr = fopen(fileName, "r");
+  if (filePtr == NULL) {
+    perror("fopen");
+    return 1;
+  }
+
+  struct fileData fData;
+  if (retrieveFileData(&fData, filePtr, fileName) != 0) {
+    printf("Error retrieving the file's data.\n");
+    return 1;
+  }
+
+  // Set up start Control Packet
+  unsigned int ctrlPacketSize =
+      CTRL_PACKET_SIZE(UNSIGNED_INT_SIZE, fData.fileNameSize);
+  unsigned char ctrlPacket[ctrlPacketSize];
+  if (generateControlPacket(ctrlPacket, &fData, PACKET_CTRL_START) != 0) {
+    printf("Error creating the start control packet.\n");
+    return 1;
+  }
+
+  // Send start Control Packet
+  if (llwrite(portfd, ctrlPacket, ctrlPacketSize) != 0) {
+    printf("Error sending start control Packet.\n");
+    return -1;
+  }
+
+  // Iterate through file, create and send Data Packets
+  unsigned int packetsSent = 0;
+  while (!feof(filePtr)) {
+    // read data
+    unsigned int dataSize =
+        (packetsSent < fData.fullPackets) ? DATA_CHUNK_SIZE : fData.leftover;
+    unsigned char data[dataSize];
+    for (size_t i = 0; i < dataSize; i++) {
+      data[i] = fgetc(filePtr);
     }
 
-    struct fileData fData;
-    if(retrieveFileData(&fData, filePtr, fileName) != 0){
-        printf("Error retrieving the file's data.\n");
-        return 1;
+    // Create Data Packet
+    unsigned int dataPacketSize = DATA_PACKET_SIZE(dataSize);
+    unsigned char dataPacket[dataPacketSize];
+    generateDataPacket(dataPacket, &fData, data, dataSize, packetsSent % 1);
+
+    // Send Data Packet
+    if (llwrite(portfd, dataPacket, dataPacketSize) != 0) {
+      printf("Error sending data packet number %u.\n", packetsSent);
+      return -1;
     }
+    packetsSent++;
+  }
 
-    //Set up start Control Packet
-    unsigned int ctrlPacketSize = CTRL_PACKET_SIZE(UNSIGNED_INT_SIZE, fData.fileNameSize);
-    unsigned char ctrlPacket[ctrlPacketSize];
-    if(generateControlPacket(ctrlPacket, &fData, PACKET_CTRL_START) != 0){
-        printf("Error creating the start control packet.\n");
-        return 1;
-    }
-    
-    //Send start Control Packet
-    if(llwrite(portfd, ctrlPacket, ctrlPacketSize) != 0){
-        printf("Error sending start control Packet.\n");
-        return -1;
-    }
+  // Set up End Control Packet
+  ctrlPacket[0] = PACKET_CTRL_END;
 
-    //Iterate through file, create and send Data Packets
-    unsigned int packetsSent = 0;
-    while(!feof(filePtr)){
-        //read data
-        unsigned int dataSize = (packetsSent < fData.fullPackets) ? DATA_CHUNK_SIZE : fData.leftover;
-        unsigned char data[dataSize];
-        for(size_t i = 0; i < dataSize; i++){
-            data[i] = fgetc(filePtr);
-        }
+  // Send End Control Packet
+  if (llwrite(portfd, ctrlPacket, ctrlPacketSize) != 0) {
+    printf("Error sending end control Packet.\n");
+    return -1;
+  }
 
-        //Create Data Packet
-        unsigned int dataPacketSize = DATA_PACKET_SIZE(dataSize);
-        unsigned char dataPacket[dataPacketSize];
-        generateDataPacket(dataPacket, &fData, data, dataSize, packetsSent % 1);
-
-        //Send Data Packet
-        if(llwrite(portfd, dataPacket, dataPacketSize) != 0){
-            printf("Error sending data packet number %u.\n", packetsSent);
-            return -1;
-        }
-        packetsSent++;
-    }
-    
-    //Set up End Control Packet
-    ctrlPacket[0] = PACKET_CTRL_END;
-
-    //Send End Control Packet
-    if(llwrite(portfd, ctrlPacket, ctrlPacketSize) != 0){
-        printf("Error sending end control Packet.\n");
-        return -1;
-    }
-
-    return 0;
+  return 0;
 }
 
-int receive(int portfd){
-    //read start packet
+int receive(int portfd) {
+  // read start packet
 
-    //create file with start packet values
+  // create file with start packet values
 
-    //while loop to read and store file until we find the end packet
+  // while loop to read and store file until we find the end packet
 
-    //close
+  // close
 
-    return 0;
+  return 0;
 }
--- src/appl.h
+++ src/appl.h
@@ -2,58 +2,61 @@
 #define _RCOM_APPL_H_
 
 #include <stdbool.h>
+#include <stdio.h>
 
-#define DATA_CHUNK_SIZE             1024
-#define DATA_PACKET_SIZE(dSize)     dSize + 4
-#define CTRL_PACKET_SIZE(l1, l2)    5+l1+l2
+#define DATA_CHUNK_SIZE 1024
+#define DATA_PACKET_SIZE(dSize) dSize + 4
+#define CTRL_PACKET_SIZE(l1, l2) 5 + l1 + l2
 
-#define PACKET_DATA         1
-#define PACKET_CTRL_START   2
-#define PACKET_CTRL_END     3
-#define CTRL_FILE_SIZE      0
-#define CTRL_FILE_NAME      1
-#define UNSIGNED_INT_SIZE   4
+#define PACKET_DATA 1
+#define PACKET_CTRL_START 2
+#define PACKET_CTRL_END 3
+#define CTRL_FILE_SIZE 0
+#define CTRL_FILE_NAME 1
+#define UNSIGNED_INT_SIZE 4
 
-struct fileData{
-    FILE *filePtr;              //Pointer to the file
-    char *fileName;             //Name of the file
-    unsigned int fileNameSize;  //Size of the file name in bytes
-    unsigned int fileSize;      //Size of the file in bytes
-    unsigned int fullPackets;   //Number of full packets we can send
-    unsigned int leftover;      //Size of the last non-full packet in bytes
+struct fileData {
+  FILE *filePtr;             // Pointer to the file
+  char *fileName;            // Name of the file
+  unsigned int fileNameSize; // Size of the file name in bytes
+  unsigned int fileSize;     // Size of the file in bytes
+  unsigned int fullPackets;  // Number of full packets we can send
+  unsigned int leftover;     // Size of the last non-full packet in bytes
 };
 
 /**
  * @brief Retrieves file data
- * 
+ *
  * @return 0 on success, -1 otherwise
  */
 int retrieveFileData(struct fileData *fData, FILE *filePtr, char *fileName);
 
 /**
  * @brief Generates a control packet from a file's data
- * 
+ *
  * @param ctrlPacket    Pointer to the packet where we're storing data
  * @param fData         Struct holding the file's data
  * @param start         Which Control Packet to generate
  * @return 0 on success, -1 otherwise
  */
-int generateControlPacket(unsigned char *ctrlPacket, struct fileData *fData, int start);
+int generateControlPacket(unsigned char *ctrlPacket, struct fileData *fData,
+                          int start);
 
 /**
  * @brief Generates a data packet from a file's content
- * 
+ *
  * @param dataPacket    Pointer to the packet where we're storing data
  * @param fData         Misc filedata
  * @param data          Data we're storing in the packet
  * @param dataSize      Size of the data in the packet
  * @param seqN          Sequence number
  */
-void generateDataPacket(unsigned char *dataPacket, struct fileData *fData, unsigned char *data, unsigned int dataSize, int seqN);
+void generateDataPacket(unsigned char *dataPacket, struct fileData *fData,
+                        unsigned char *data, unsigned int dataSize, int seqN);
 
 /**
  * @brief Creates packets from file and sends them to the receiver
- * 
+ *
  * @param portfd    File Descriptor of the serial port
  * @param fileName  Name of the file to send
  */
@@ -61,7 +64,7 @@ int write(int portfd, char *fileName);
 
 /**
  * @brief Receives packets from the transmitter and decodes them
- * 
+ *
  * @param portfd    File Descriptor of the serial port
  */
 int receive(int portfd);
