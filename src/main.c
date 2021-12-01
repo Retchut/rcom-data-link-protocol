370fb0c62f88e2f42dc2618919d691e2a2664cb4
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-12-01 19:49:58 +0000

    Adds stub for llread

--- src/main.c
+++ src/main.c
@@ -1,31 +1,58 @@
-#include <fcntl.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
-#include <termios.h>
-#include <unistd.h>
 
 #include "defines.h"
 #include "ll.h"
+#include "send.h"
 
 int main(int argc, char *argv[]) {
 
-  if (!(argc == 2 && strcmp(argv[1], "receiver") == 0) &&
-      !(argc == 3 && strcmp(argv[1], "emitter") == 0)) {
+  if (!(argc == 3 && strcmp(argv[1], "receiver") == 0) &&
+      !(argc == 4 && strcmp(argv[1], "emitter") == 0)) {
     fprintf(stderr, "Usage:\t./recv SerialPort File\n\tex: nserial /dev/tty11 "
                     "pinguim.jpg\n");
     exit(-1);
   }
 
   bool role = -1;
+  int port = -1;
   if (strcmp(argv[1], "emitter") == 0) {
     role = TRANSMITTER;
   } else if (strcmp(argv[1], "receiver") == 0) {
     role = RECEIVER;
   }
 
-  llopen(serial_port_fd, role);
+  if (isdigit(argv[2][0]) && atoi(argv[2]) >= 0) {
+    port = atoi(argv[2]);
+  } else {
+    fprintf(stderr, "Invalid port specified\n");
+    exit(-1);
+  }
+
+  int fd = -1;
+  if ((fd = llopen(port, role)) == -1) {
+    fprintf(stderr, "Error opening port \n");
+    exit(-1);
+  }
+
+  unsigned char buf[5] = "ABCDE";
+
+  if (role == TRANSMITTER) {
+    if (llwrite(fd, buf, 5) == -1) {
+      fprintf(stderr, "Error writing to port \n");
+      exit(-1);
+    }
+  } else if (role == RECEIVER) {
+
+    writeSupervisionFrame(fd, A_RECV_RSP, C_REJ(0));
+  }
+
+  if (llclose(fd) == -1) {
+    fprintf(stderr, "Error closing port \n");
+    exit(-1);
+  }
 
   return 0;
 }
