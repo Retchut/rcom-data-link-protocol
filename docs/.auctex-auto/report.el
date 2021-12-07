0e8426ef871976861ddc4b9e7b99f2fff0962489
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2021-12-07 17:58:27 +0000

    Adds a first template to the final report.

---
+++ docs/.auctex-auto/report.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "report"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "graphicx"
+    "inputenc"))
+ :latex)
+
